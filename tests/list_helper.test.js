e95393970239e193beef0b5da624672eee28a79c
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2022-07-13 23:27:17 -0300

    feat/test: testing with jest configured and several test written

---
+++ tests/list_helper.test.js
@@ -0,0 +1,210 @@
+const listHelper = require('../utils/list_helper');
+
+describe('dummy', () => {
+  test('returns one', () => {
+    expect(listHelper.dummy([])).toBe(1);
+  });
+});
+
+describe('total likes', () => {
+  const listWithNoBlogs = [];
+  const listWithOneBlog = [
+    {
+      id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithTwoBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test('when list has no blogs, equals 0', () => {
+    const result = listHelper.totalLikes(listWithNoBlogs);
+    expect(result).toBe(0);
+  });
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  test('when list has two blogs, equals the likes of the sums of its likes ', () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs);
+    expect(result).toBe(12);
+  });
+});
+
+describe('favorite blog', () => {
+  const listWithNoBlogs = [];
+  const listWithOneBlog = [
+    {
+      id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithTwoBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test('When only one blog is in the list, that blog is returned', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toEqual(listWithOneBlog[0]);
+  });
+
+  test('When only two blogs are in the list, the correct one is returned', () => {
+    const result = listHelper.favoriteBlog(listWithTwoBlogs);
+    expect(result).toEqual(listWithTwoBlogs[0]);
+  });
+});
+
+describe('most blogs', () => {
+  const listWithOneBlog = [
+    {
+      id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    },
+  ];
+
+  test('with one author, it gets returned', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog);
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      blogs: 1,
+    });
+  });
+
+  test('with two authors, the correct one is returned', () => {
+    const result = listHelper.mostBlogs(listWithThreeBlogs);
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      blogs: 2,
+    });
+  });
+});
+
+describe('most likes', () => {
+  const listWithOneBlog = [
+    {
+      id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    },
+  ];
+
+  test('with one author, it gets returned', () => {
+    const result = listHelper.mostLikes(listWithOneBlog);
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    });
+  });
+
+  test('with two authors, the correct one is returned', () => {
+    const result = listHelper.mostLikes(listWithThreeBlogs);
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 17,
+    });
+  });
+});
---
+++ utils/list_helper.js
@@ -0,0 +1,68 @@
+function dummy(blogs) {
+  return 1;
+}
+
+function totalLikes(blogs) {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+}
+
+function favoriteBlog(blogs) {
+  return blogs.reduce(
+    (favBlog, currBlog) =>
+      currBlog.likes > favBlog.likes ? currBlog : favBlog,
+    { likes: 0 }
+  );
+}
+
+function mostBlogs(blogs) {
+  let blogsByAuthor = [];
+  blogs.forEach((blog) => {
+    const foundObj = blogsByAuthor.find((b) => b.author === blog.author);
+    if (foundObj) {
+      blogsByAuthor = blogsByAuthor.map((b) =>
+        b.author === foundObj.author
+          ? { author: b.author, blogs: b.blogs + 1 }
+          : b
+      );
+    } else {
+      blogsByAuthor = blogsByAuthor.concat({ author: blog.author, blogs: 1 });
+    }
+  });
+
+  return blogsByAuthor.reduce((mostBlogsAuthor, author) =>
+    mostBlogsAuthor.blogs < author.blogs ? author : mostBlogsAuthor
+  );
+}
+
+function mostLikes(blogs) {
+  let likesByAuthor = [];
+  blogs.forEach((blog) => {
+    const foundObj = likesByAuthor.find((b) => b.author === blog.author);
+    if (foundObj) {
+      likesByAuthor = likesByAuthor.map((b) =>
+        b.author === foundObj.author
+          ? { author: b.author, likes: b.likes + blog.likes }
+          : b
+      );
+    } else {
+      likesByAuthor = likesByAuthor.concat({
+        author: blog.author,
+        likes: blog.likes,
+      });
+    }
+  });
+
+  console.log(likesByAuthor);
+
+  return likesByAuthor.reduce((mostLikesAuthor, author) =>
+    mostLikesAuthor.likes < author.likes ? author : mostLikesAuthor
+  );
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
